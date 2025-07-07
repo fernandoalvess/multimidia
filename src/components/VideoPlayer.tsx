d4ea12aa1c1010b50762c51cf3f4b13fa7d90d9e
fernandoalvess <fernandoumalves@gmail.com>
2025-07-07 14:33:44 -0300

    ok

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,130 @@
+// src/components/VideoPlayer.tsx
+'use client';
+
+import { useState, useRef } from 'react';
+import styles from './VideoPlayer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faBackward, faForward, faVolumeUp, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
+
+const VideoPlayer = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const progressRef = useRef<HTMLDivElement>(null);
+
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+
+    const formatTime = (timeInSeconds: number) => {
+        const minutes = Math.floor(timeInSeconds / 60);
+        const seconds = Math.floor(timeInSeconds % 60);
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    };
+
+    const togglePlayPause = () => {
+        if (videoRef.current) {
+            if (isPlaying) {
+                videoRef.current.pause();
+            } else {
+                videoRef.current.play();
+            }
+            setIsPlaying(!isPlaying);
+        }
+    };
+
+    const skip = (amount: number) => {
+        if (videoRef.current) {
+            videoRef.current.currentTime += amount;
+        }
+    };
+
+    const toggleMute = () => {
+        if (videoRef.current) {
+            videoRef.current.muted = !isMuted;
+            setIsMuted(!isMuted);
+        }
+    };
+
+    const handleLoadedMetadata = () => {
+        if (videoRef.current) {
+            setDuration(videoRef.current.duration);
+        }
+    };
+
+    const handleTimeUpdate = () => {
+        if (videoRef.current) {
+            if (duration == 0 && videoRef.current.duration) {
+                setDuration(videoRef.current.duration);
+            }
+            
+            setCurrentTime(videoRef.current.currentTime);
+        }
+    };
+
+    const handleProgressSeek = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (progressRef.current && videoRef.current) {
+            const progressRect = progressRef.current.getBoundingClientRect();
+            const seekPosition = (e.clientX - progressRect.left) / progressRect.width;
+            videoRef.current.currentTime = seekPosition * duration;
+        }
+    };
+
+    const progressPercentage = duration > 0 ? (currentTime / duration) * 100 : 0;
+
+    return (
+        <div className={styles.containerDoPlayer}>
+            <div className={styles.infoSuperior}>
+                <span className={styles.identificadorSuperior}>PLAYER DE VÍDEO</span>
+            </div>
+
+            <video
+                ref={videoRef}
+                className={styles.telaDoVideo}
+                poster="/assets/manoelgomes.jpeg"
+                onClick={togglePlayPause}
+                onLoadedMetadata={handleLoadedMetadata}
+                onTimeUpdate={handleTimeUpdate}
+                onPlay={() => setIsPlaying(true)}
+                onPause={() => setIsPlaying(false)}
+            >
+                <source src="/assets/videomanoelgomes.mp4" type="video/mp4" />
+                Seu navegador não suporta a tag de vídeo.
+            </video>
+
+            <div className={styles.informacoesDaMusica}>
+                <h2 className={styles.tituloDaMusica}> Ceiça - O Clipe</h2>
+                <p className={styles.nomeDoArtista}>Manoel Gomes</p>
+            </div>
+
+            <div
+                ref={progressRef}
+                className={styles.containerBarraDeProgresso}
+                onClick={handleProgressSeek}
+            >
+                <div className={styles.barraDeProgresso} style={{ width: `${progressPercentage}%` }}></div>
+            </div>
+
+            <div className={styles.marcacoesDeTempo}>
+                <span className={styles.tempoAtual}>{formatTime(currentTime)}</span>
+                <span className={styles.tempoTotal}>{formatTime(duration)}</span>
+            </div>
+
+            <div className={styles.controles}>
+                <button className={styles.botaoDeControle} onClick={() => skip(-10)}>
+                    <FontAwesomeIcon icon={faBackward} />
+                </button>
+                <button className={`${styles.botaoDeControle} ${styles.botaoDePlay}`} onClick={togglePlayPause}>
+                    <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+                </button>
+                <button className={styles.botaoDeControle} onClick={() => skip(10)}>
+                    <FontAwesomeIcon icon={faForward} />
+                </button>
+                <button className={styles.botaoDeControle} onClick={toggleMute}>
+                    <FontAwesomeIcon icon={isMuted ? faVolumeMute : faVolumeUp} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default VideoPlayer;
